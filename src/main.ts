f4d152f2fdc39749ffebdb3473b7aadbec53fca8
David Agudelo <agudeloocampodavid@gmail.com>
2026-03-09 22:43:45 -0500

    swager

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,14 @@ whitelist: true,
 forbidNonWhitelisted: true,
 })
 );
+const config = new DocumentBuilder()
+    .setTitle('Tienda Tdea')
+    .setDescription('Proyecto Tienda Tdea')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
