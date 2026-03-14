4deba688b8d4b1272a511c23520d1fa62d0d1078
David Agudelo <agudeloocampodavid@gmail.com>
2026-03-13 22:33:42 -0500

    render 3000

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+});
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
